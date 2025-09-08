6fea198aaa55cfc3ef652984538215d5fb781613
HANNA <vhanna659@gmail.com>
2025-09-08 07:03:17 +0530

    Add files via upload

---
+++ 08-09-25/ScatterPlot(Iris Dataset).py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot
+import pandas
+df = pandas.read_csv("iris.csv")
+fig, ax = pyplot.subplots(1,1)
+colors = {'setosa':'red','versicolor':'green','virginica':'blue'}
+grouped = df.groupby('species')
+
+for key, group in grouped:
+    group.plot(ax=ax,kind='scatter',x='petal.length',y='petal.width',label=key,color=colors[key])
+ax.set_title("Scatter Plot")
+ax.set_xlabel("Petal Length")
+ax.set_ylabel("Petal Width")
+pyplot.show()
